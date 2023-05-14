4c0bfa6e84e0a40710bed4c86e1e8c8642b3b2f5
David Fekke <david@fekke.com>
2023-05-14 16:32:40 -0400

    Adding completion example

---
+++ completions.js
@@ -0,0 +1,17 @@
+import { Configuration, OpenAIApi } from 'openai';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+const response = await openai.createCompletion({
+  model: "text-davinci-003",
+  prompt: "Say this is a test",
+  max_tokens: 7,
+  temperature: 0,
+});
+
+const json = await response;
+console.log(json.data.choices);
